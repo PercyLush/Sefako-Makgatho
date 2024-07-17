56b06343ae498aea132bac26016ae0482ab57f00
Bheki Lushaba <bheki@gradesmatch.com>
2024-07-17 15:36:39 +0200

    done sefako makgatho

--- compare_and_append.py
+++ compare_and_append.py
@@ -1,6 +1,6 @@
 import json
 
-path1 = "C:\\Users\\Bheki Lushaba\\Desktop\\Sefako Makgatho\\BDSandBOH.json"
+path1 = "C:\\Users\\Bheki Lushaba\\Desktop\\Sefako Makgatho\\OralHealthFINALs.json"
 path2 = "C:\\Users\\Bheki Lushaba\\Desktop\\Sefako Makgatho\\OralHealth.json"
 
 with open(path1, "r", encoding="utf-8") as file1, open(path2, "r", encoding="utf-8") as file2:
@@ -11,12 +11,12 @@ with open(path1, "r", encoding="utf-8") as file1, open(path2, "r", encoding="utf
         if "Code" in item1:
             for item2 in data2:
                 if "Code" in item2:
-                    if item1["Code"] in item2["Code"]:
-                        for key in ["Description", "Prerequisite", "Corequisite","Assessments"]:
+                    if item1["Code"] == item2["Code"]:
+                        for key in ["Description"]:
                             if key in item2:
                                 item1[key] = item2.get(key)
                     else:
                         pass
 
-with open("C:\\Users\\Bheki Lushaba\\Desktop\\Sefako Makgatho\\OralHealthFinal.json", "w", encoding="utf-8") as file:
+with open("C:\\Users\\Bheki Lushaba\\Desktop\\Sefako Makgatho\\OralHealthFINALs.json", "w", encoding="utf-8") as file:
     json.dump(data1, file, indent=2)
